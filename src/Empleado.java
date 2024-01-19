a4291b4fc33e224aedf48a2a9ba917a62a702a47
Juan Carlos <jdemanuel7@gmail.com>
2024-01-19 19:55:29 +0100

    a

--- src/Empleado.java
+++ src/Empleado.java
@@ -18,9 +18,15 @@ public class Empleado {
     private string nombre;
     private  double salarioBase;
 
-    public static  calcularSalario{
-
+    public double calcularSalario() {
+        return salarioBase;
     }
+    public string getNombre() {
+        return nombre;
+    }
+}
+
+
 
 
 
