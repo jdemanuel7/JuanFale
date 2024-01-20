44ac8ea623c3c59f682a3646da17dc17afef9a6b
Juan Carlos <jdemanuel7@gmail.com>
2024-01-20 11:18:20 +0100

    Actualizacion de clases

--- src/Empleado.java
+++ src/Empleado.java
@@ -1,33 +1,34 @@
 public class Empleado {
-    private string nombre;
+    private String nombre;
     private double salarioBase;
 
-    // Constructor que da el salario y nombre
-    public Empleado(string nombre, double salarioBase) {
+    // Constructor que establece el salario y nombre
+    public Empleado(String nombre, double salarioBase) {
         this.nombre = nombre;
         this.salarioBase = salarioBase;
     }
 
-    // Método que devuelve salario base
+    // Método que devuelve el salario base
     public double calcularSalario() {
         return salarioBase;
     }
 
     // Getter para obtener el nombre del empleado
-    public string getNombre() {
+    public String getNombre() {
         return nombre;
     }
 
     // Setter para actualizar el nombre del empleado
-    public void setNombre(string nombre) {
+    public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    // Getter para obtener el salario base
     public double getSalarioBase() {
         return salarioBase;
     }
 
-
+    // Setter para actualizar el salario base
     public void setSalarioBase(double salarioBase) {
         this.salarioBase = salarioBase;
     }
--- src/EmpleadoAsalariado.java
+++ src/EmpleadoAsalariado.java
@@ -1,4 +1,22 @@
-public class EmpleadoAsalariado {
+public class EmpleadoAsalariado extends Empleado {
+    private double bonoAnual;
 
+    // Constructor  nombre, salario base y bono anual
+    public EmpleadoAsalariado(String nombre, double salarioBase, double bonoAnual) {
+        // Llamo al constructor de empleado
+        super(nombre, salarioBase);
+        this.bonoAnual = bonoAnual;
+    }
+
+    // Sobrescribe el método calcularSalario para incluir el bono anual
+    public double calcularSalario() {
+        // Llamo al metodo de empleado y agrego el bono anual
+        return super.calcularSalario() + bonoAnual;
+    }
+    public double getBonoAnual() {
+        return bonoAnual;
+    }
+    public void setBonoAnual(double bonoAnual) {
+        this.bonoAnual = bonoAnual;
+    }
 }
-//comentario juan
